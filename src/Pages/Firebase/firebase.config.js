d411913b3da68aa0c10780899526306245d0a029
Bipro <biprobarai18@gmail.com>
2022-03-29 09:33:18 +0600

    firebase

--- src/Pages/Firebase/firebase.config.js
+++ src/Pages/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBCPlqFsOq1gdN-0yqdODyLtv0epkwm-lU",
-    authDomain: "ed-tech-site.firebaseapp.com",
-    projectId: "ed-tech-site",
-    storageBucket: "ed-tech-site.appspot.com",
-    messagingSenderId: "1091591429455",
-    appId: "1:1091591429455:web:f9e22d494f06412338b93a",
-    measurementId: "G-2CPN0GPFKZ"
+    apiKey: "AIzaSyDNKakvzGXCSq9fLp7DGDP5UY8omRqonE4",
+    authDomain: "ed-tech-site-164bc.firebaseapp.com",
+    projectId: "ed-tech-site-164bc",
+    storageBucket: "ed-tech-site-164bc.appspot.com",
+    messagingSenderId: "141734247040",
+    appId: "1:141734247040:web:3373c2fd6496058b934465",
+    measurementId: "G-T0RM91BT04"
 };
 export default firebaseConfig;
